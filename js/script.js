7445d1806d3b5387880573e14f41f233360cd2f1
alex5667 <alex56671111@gmail.com>
2022-08-03 22:36:43 +0300

    feat: start add bg

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const nameU = document.querySelector('.name');
+const body = document.getElementsByTagName('body');
+let randomNum;
+
 
 
 function showTime() {
@@ -24,27 +27,39 @@ function showDate() {
 function getTimeOfDay() {
     const greet = new Date();
     let hours = greet.getHours();
-    let  currgreeting;
-    if (hours < 5)  currgreeting = "Good night"; 
-        else if (hours < 12)  currgreeting = "Good morning";
-        else if (hours < 18) currgreeting = "Good afternoon";
-        else if (hours < 24) currgreeting = "Good evening"; 
-        else currgreeting = "Good night";
-        greeting.textContent = currgreeting;
+    if (hours < 5) currgreeting = "Good night";
+    else if (hours < 12) currgreeting = "Good morning";
+    else if (hours < 18) currgreeting = "Good afternoon";
+    else if (hours < 24) currgreeting = "Good evening";
+    else currgreeting = "Good night";
+    greeting.textContent = currgreeting;
+    return currgreeting;
 }
 
 function setLocalStorage() {
     let name = nameU;
     localStorage.setItem('name', name.value);
-  }
-  window.addEventListener('beforeunload', setLocalStorage)
+}
+window.addEventListener('beforeunload', setLocalStorage)
 
-  function getLocalStorage() {
-    if(localStorage.getItem('name')) {
-     let name = nameU;
-  
-      name.value = localStorage.getItem('name');
+function getLocalStorage() {
+    if (localStorage.getItem('name')) {
+        let name = nameU;
+        name.value = localStorage.getItem('name');
     }
-  }
-  window.addEventListener('load', getLocalStorage)
+}
+window.addEventListener('load', getLocalStorage)
+
+function getRandomNum() {
+    min = 1;
+    max = 20;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function setBg() {
+    let timeOfDay = getTimeOfDay();
+    let bgNum = getRandomNum().toString().padStart(2, "0");
+    let urlbg = `\"url(\'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/${bgNum}.jpg\')\"`;
+    body.style.backgroundImage = urlbg;
+}
+setBg();
 
